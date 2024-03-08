840e732bdf02258e957c20473119f2ba8c0f21c9
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2024-03-09 02:14:46 +0530

    Created uploading functionality using shadcn and uploadthing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,15 +1,29 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
  
 const f = createUploadthing();
  
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
     .middleware(async ({ req }) => {
-      return {};
+        const {getUser} = getKindeServerSession()
+        const user = await getUser()
+        if(!user || !user.id) throw new Error("Unauthorized")
+
+        return {userId:user.id};
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      return {};
+      const createdFile = await db.file.create({
+        data:{
+          key:file.key,
+          name:file.name,
+          userId:metadata.userId,
+          url:file.url,
+          uploadStatus:"PROCESSING",
+        }
+      })
+      
     }),
 } satisfies FileRouter;
  
