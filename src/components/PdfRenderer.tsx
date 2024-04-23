a5d0ba529db3435355d00836b97439eb05615753
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2024-04-24 03:25:03 +0530

    added zoom button in pdf feature bar

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2, Search } from "lucide-react";
 import {Document, Page, pdfjs} from "react-pdf"
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';
@@ -10,10 +10,12 @@ import {useResizeDetector} from 'react-resize-detector';
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useState } from "react";
-import {useForm} from 'react-hook-form';
+import {useForm} from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { cn } from "@/lib/utils";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import SimpleBar from 'simplebar-react';
 
 pdfjs.GlobalWorkerOptions.workerSrc =  `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -27,6 +29,7 @@ const PdfRenderer = ({url}:PdfRendererProps)=>{
 
     const [numPages, setNumPages] = useState<number>(0)
     const [currPage, setCurrPage] = useState<number>(1)
+    const [scale, setScale] = useState<number>(1)
 
     const CustomPageValidator = z.object({
         page: z.string().refine((num)=> Number(num) > 0 && Number(num) <= numPages!)
@@ -57,6 +60,7 @@ const PdfRenderer = ({url}:PdfRendererProps)=>{
             {/* Pdf options */}
             <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
                 <div className="flex items-center gap-1.5">
+{/* Right ChevronButton in pdf feature bar */}
                     <Button 
                         disabled={currPage <= 1}
                         onClick={()=>{
@@ -64,16 +68,17 @@ const PdfRenderer = ({url}:PdfRendererProps)=>{
                         }} variant='ghost' aria-label='previous page'>
                         <ChevronDown className='h-4 w-4' />
                     </Button>
+{/* Input form in pdf feature bar */}
                     <div className="flex items-center gap-1.5">
                         <Input
-                            {...register('page')}
+                            {...register("page")}
                             className={cn(
                                 'w-12 h-8',
                                 errors.page && 'focus-visible:ring-red-500'
                               )}
                             onKeyDown={(e)=>{
-                                if(e.key === "Error"){
-                                    handleSubmit(handlePageSubmit)
+                                if(e.key === "Enter"){
+                                    handleSubmit(handlePageSubmit)()
                                 }
                             }}    
                         />
@@ -82,41 +87,76 @@ const PdfRenderer = ({url}:PdfRendererProps)=>{
                             <span>{numPages ?? "x"}</span>
                         </p>
                     </div>
-
+{/* Right ChevronButton in pdf feature bar */}
                     <Button
-                    disabled={numPages === undefined || currPage === numPages}
+                    disabled={
+                        numPages === undefined || 
+                        currPage === numPages
+                        }
                         onClick={()=>{
                             setCurrPage( (prev)=> (prev+1 > numPages! ? numPages! : prev+1))
                         }}
                         variant='ghost' aria-label='next page'>
                         <ChevronUp className='h-4 w-4' />
                     </Button>
-
                 </div>
+
+{/* Zooming the pdf feature bar */}
+                <div className="space-x-4">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button
+                                className="gap-1.5"
+                                aria-label="zoom" 
+                                variant='ghost'>
+                                <Search h-4 w-4 />
+                                {scale * 100}%<ChevronDown className="h-3 w-3 opacity-50" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem onSelect={()=>setScale(1)}>
+                                100%
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onSelect={()=>setScale(1.5)}>
+                                150%
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onSelect={()=>setScale(2)}>
+                                200%
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onSelect={()=>setScale(2.5)}>
+                                250%
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>    
+                </div>
+
+                
             </div>
 
             <div className="flex-1 w-full max-h-screen">
-                <div ref={ref}> 
-                    <Document 
-                        loading={   
-                            <div className="flex justify-center">
-                                <Loader2 className="my-24 h-6 w-6 animate-spin"/>
-                            </div>
-                        } 
-                        onLoadError={()=>{
-                            toast({
-                                title:'Error loading pdf',
-                                description:'Please try again after some time',
-                                variant:'destructive'
-                            })
-                        }}
-                        onLoadSuccess={({numPages})=>{
-                            setNumPages(numPages)
-                        }}
-                        file={url} className='max-h-full'>
-                            <Page width={width? width:1} pageNumber={currPage} />
-                    </Document>
-                </div>
+                <SimpleBar autoHide={false} className="max-h-[calc(100vh-10rem)]">
+                    <div ref={ref}> 
+                        <Document 
+                            loading={   
+                                <div className="flex justify-center">
+                                    <Loader2 className="my-24 h-6 w-6 animate-spin"/>
+                                </div>
+                            } 
+                            onLoadError={()=>{
+                                toast({
+                                    title:'Error loading pdf',
+                                    description:'Please try again after some time',
+                                    variant:'destructive'
+                                })
+                            }}
+                            onLoadSuccess={({numPages})=>{
+                                setNumPages(numPages)
+                            }}
+                            file={url} className='max-h-full'>
+                                <Page width={width? width:1} pageNumber={currPage} scale={scale}/>
+                        </Document>
+                    </div>
+                </SimpleBar>
             </div>
         </div>
     )
