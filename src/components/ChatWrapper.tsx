56691e778c11ba63b45ba1d5650e37723b0addf1
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2024-04-24 23:20:15 +0530

    Starting creation of chat wrapper

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -1,5 +1,11 @@
+import Messages from "./Messages"
+
 const ChatWrapper = ()=>{
-    return <div>chat</div>
+    return <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+        <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+            <Messages/>
+        </div>
+    </div>
 }
 
 export default ChatWrapper
